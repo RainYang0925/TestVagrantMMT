70dfd1ab77749e285288a8ceff0483a2b02a3661
Raman <Raman@Raman-PC>
2017-01-20 20:05:45 +0530

    POM class DOM elements identification have been completed with methods.

--- MakeMyTripTV/src/test/java/PageObject_Component/PageObjectSearch.java
+++ MakeMyTripTV/src/test/java/PageObject_Component/PageObjectSearch.java
@@ -2,7 +2,10 @@ package PageObject_Component;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+
+import Generic_Component.BaseClass;
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidKeyCode;
 import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 
@@ -42,17 +45,68 @@ public class PageObjectSearch {
 
 	@AndroidFindBy(id = "com.makemytrip:id/calOK")
 	public WebElement departureDateOK;
-	
+
 	@AndroidFindBy(id = "com.makemytrip:id/returnDateLayout")
 	public WebElement returnDate;
 
 	@AndroidFindBy(name = "25")
 	public WebElement returnDateSelection;
-	
+
 	@AndroidFindBy(id = "com.makemytrip:id/travellerTextID")
 	public WebElement adult_count;
+
+	//Assuming Adult = 1
+	@AndroidFindBy(id = "com.makemytrip:id/okPax")
+	public WebElement adult_countOK;
+
+	@AndroidFindBy(id = "com.makemytrip:id/classTextID")
+	public WebElement class_Selection;
+
+	//Class Category
+	@AndroidFindBy(id = "com.makemytrip:id/economyClass")
+	public WebElement class_Type;
+
+	//Search Flights
+	@AndroidFindBy(id = "com.makemytrip:id/searchFlights")
+	public WebElement seach_Flights;
+
+	//Assuming default price is OK, Booking Initiated..
+	@AndroidFindBy(id = "com.makemytrip:id/split_book_btn")
+	public WebElement click_Booking;
+
+	//Continue
+	@AndroidFindBy(id = "com.makemytrip:id/continue_text")
+	public WebElement click_Continue;
+
+	//Add Adult
+	@AndroidFindBy(id = "com.makemytrip:id/traveller_add_new_pax_text")
+	public WebElement add_Adult;
+
+	//Adding Adult Details
+	@AndroidFindBy(id = "com.makemytrip:id/pax_first_name")
+	public WebElement first_Name;
+
+	@AndroidFindBy(id = "com.makemytrip:id/pax_last_name")
+	public WebElement last_Name;
+
+	//Done Button
+	@AndroidFindBy(id = "com.makemytrip:id/pax_add_traveller_button")
+	public WebElement click_Done;
+
+	//Done Button
+	@AndroidFindBy(id = "com.makemytrip:id/book_flight_text_view")
+	public WebElement click_BookFlight;
+
+	//Agree and Continue
+	@AndroidFindBy(id = "com.makemytrip:id/flight_agree_insurance_button")
+	public WebElement click_AgreeContinue;
+
+	//Continue with final fare details
+	@AndroidFindBy(id = "com.makemytrip:id/button_fare_change_continue")
+	public WebElement click_FinalContinue;
 	
 
+	//---------------------------------------------------------------
 
 	public void flight_search() {
 		flghts.click(); 
@@ -122,7 +176,7 @@ public class PageObjectSearch {
 		Thread.sleep(2000);
 	}
 
-	
+
 	/*
 	 * Return Date selection, Here assumed Date is 25th Jan'17
 	 * 
@@ -132,7 +186,7 @@ public class PageObjectSearch {
 		returnDate.click();
 		Thread.sleep(2000);
 	}
-	
+
 	public void click_returnDate() {
 		returnDateSelection.click(); 
 	}
@@ -142,14 +196,119 @@ public class PageObjectSearch {
 		departureDateOK.click(); 
 		Thread.sleep(2000);
 	}
-	
-	
+
+
 	//Adult and Class selection
 	public void select_AdultCnt() throws InterruptedException {
 		adult_count.click(); 
 		Thread.sleep(3000);
 	}
+
+	//Taking adult count As 1
+	public void adultCount_OK() {
+		adult_countOK.click(); 
+	}
+
+	//Class Category
+
+	public void click_ClassType() {
+		class_Selection.click(); 
+	}
+
+	//Assuming Economy Class is our Test Data
+	public void click_ClassTypeSelected() {
+		class_Type.click(); 
+	}
+
+	//Search Flights
+
+	public void search_Flights() throws InterruptedException {
+		seach_Flights.click(); 
+		Thread.sleep(10000);
+	}
+
+	/*
+	 * ----------------------------------------------------------------------
+	 * Assuming default price selection among the search result is OK and
+	 * Initiating Booking Process
+	 * ----------------------------------------------------------------------
+	 */
+
+	public void booking_Init() throws InterruptedException {
+		click_Booking.click(); 
+		Thread.sleep(6000);
+	}
+
+	/*
+	 * --------------------------------------------------------------
+	 * Assuming default Insurance selection is OK
+	 * --------------------------------------------------------------
+	 */
+
+	public void click_ContinueBooking() throws InterruptedException {
+		click_Continue.click(); 
+		Thread.sleep(6000);
+	}
+
+	/*
+	 * ----------------------------------------------------
+	 * Adding Adult Details as following Test Data
+	 * First Name: Raman
+	 * Last Name : Hullur
+	 * Gender: Male
+	 * --------------------------------------------------------
+	 */
+
+	public void click_AddNewAdult() throws InterruptedException {
+		add_Adult.click(); 
+		Thread.sleep(2000);
+	}
+
+	public void enter_FirstLastName(String fname, String lname) throws InterruptedException {
+		first_Name.click();
+		Thread.sleep(2000);
+
+		//Handling Soft Keyboard Popup
+		BaseClass.driver.sendKeyEvent(AndroidKeyCode.BACK);
+		first_Name.sendKeys(fname);
+
+		last_Name.click();
+		Thread.sleep(2000);
+
+		//Handling Soft Keyboard Popup
+		BaseClass.driver.sendKeyEvent(AndroidKeyCode.BACK);
+		last_Name.sendKeys(lname);
+
+		//Gender TD default: Male
+
+	}
+
+	public void click_DoneButton() throws InterruptedException {
+		click_Done.click(); 
+		Thread.sleep(2000);
+	}
+
+	public void click_BookFlightButton() throws InterruptedException {
+		click_BookFlight.click(); 
+		Thread.sleep(2000);
+	}
+
+	public void click_AgreeContinueButton() throws InterruptedException {
+		click_AgreeContinue.click(); 
+		Thread.sleep(2000);
+	}
 	
+	public void click_ContinuePriceButton() throws InterruptedException {
+		click_FinalContinue.click(); 
+		Thread.sleep(2000);
+	}
 	
+	//Adult details are not added initially. Hence, User reaches to this activity again
+	public void click_BookFlightButton2() throws InterruptedException {
+		click_BookFlight.click(); 
+		Thread.sleep(2000);
+	}
 	
+	//Airlines Server error for selected Test Data(Travel Date, Random Passenger Details).........
+
 }
