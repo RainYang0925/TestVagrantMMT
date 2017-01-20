ad59e36b8076a91440116784b6b45b0c96170fdc
Raman <Raman@Raman-PC>
2017-01-20 18:10:14 +0530

    Driver class and DataProvider(ignore it) class created as part of POM.

---
+++ MakeMyTripTV/src/test/java/DataProvider_Component/DataProviderClass.java
@@ -0,0 +1,13 @@
+package DataProvider_Component;
+
+
+public class DataProviderClass { 
+	
+	/*
+	 * Ignore this class, Only for future purpose 
+	 * -----------------------------------
+	 * Not taking any test data from test validation dynamic data from Excel file
+	 * 
+	 */
+
+}
---
+++ MakeMyTripTV/src/test/java/Scenario_Component/DriverComponent.java
@@ -0,0 +1,13 @@
+package Scenario_Component;
+
+import Generic_Component.BaseClass;
+
+/*
+ * Driver Class 
+ */
+
+public class DriverComponent extends BaseClass{
+	
+	//Not yet started
+
+}
