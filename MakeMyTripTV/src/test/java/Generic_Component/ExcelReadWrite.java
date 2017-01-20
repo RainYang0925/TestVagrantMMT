7bba2cf7b2e2edc449c6b2d52789a091c8406d16
Raman <Raman@Raman-PC>
2017-01-20 20:08:43 +0530

    ExcelReadWrite class has created as a part of FW design and not used

---
+++ MakeMyTripTV/src/test/java/Generic_Component/ExcelReadWrite.java
@@ -0,0 +1,11 @@
+package Generic_Component;
+
+public class ExcelReadWrite {
+	
+	/*
+	 * Not using this class due to absence of Test Case and Test Data Files
+	 * Creates as a part of FrameWork Design
+	 * 
+	 */
+
+}
