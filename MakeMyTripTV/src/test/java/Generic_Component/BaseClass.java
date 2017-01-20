3b5160ff645819a2983cee8b17c28c5613ebd598
Raman <Raman@Raman-PC>
2017-01-20 16:04:11 +0530

    method for creating a folder to store captured screenshot with its name.

--- MakeMyTripTV/src/test/java/Generic_Component/BaseClass.java
+++ MakeMyTripTV/src/test/java/Generic_Component/BaseClass.java
@@ -1,10 +1,17 @@
 package Generic_Component;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import io.appium.java_client.android.AndroidDriver;
 
@@ -12,6 +19,8 @@ public class BaseClass {
 
 	public static Process process;
 	public static AndroidDriver driver;
+	public static String folder;
+	public static DateFormat dateformat;
 
 	//Start server
 	public static void Start_Server() throws IOException, InterruptedException{
@@ -74,5 +83,25 @@ public class BaseClass {
 		//WebDriverWait wait = new WebDriverWait(driver, 25);
 		driver.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);
 
+	}
+	
+	/*
+	 *Setting Format for snapshot file and creating folder name with file name for 
+	 *each screenshot then copying the file into created 'folder' directory 
+	 */
+	
+	public static void snapShot_MMT() throws IOException{
+		
+		folder = "screenshot";
+        
+		File f=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        
+        dateformat=new  SimpleDateFormat("dd-MMM-yyyy__hh_mm_ssaa");
+        
+        new File(folder).mkdir();
+      
+        String file_name=dateformat.format(new Date())+".png";
+       
+        FileUtils.copyFile(f, new File(folder + "/" + file_name));
 	}
 }
